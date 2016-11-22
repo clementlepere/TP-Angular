2c408a45dd286390bbc00040d4512c4233564b5f
Tony2Madrid <anto.meiye@gmail.com>
2016-11-22 11:35:28 +0100

    2nd

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ pizza.controller('pizzaController', function($scope){
 	$scope.types = types;
 	$scope.pates = pates;
 	$scope.extras = extras;
+
+	$scope.choice = {slice: 6, types: 8, pates: 0, extras: []};
+
 	$scope.step = 1;
 	$scope.adressLine = 1;
-
-	$scope.choice = {slice: 6, types, pates, extras};
-
-});
+}); 
