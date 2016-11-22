abd76f4b728bf55114bf6a609538d5c191464728
Tony2Madrid <anto.meiye@gmail.com>
2016-11-22 11:23:35 +0100

    1st

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var pizza = angular.module('pizza', []);
 
-pizza.controller('pizzaController', function(){
-  $scope.step = 1;
-  $scope.adressLine = 1;
-  $scope.types = types;
-  $scope.pates = pates;
-  $scope.extras = extras;
+pizza.controller('pizzaController', function($scope){
+
+	$scope.types = types;
+	$scope.pates = pates;
+	$scope.extras = extras;
+	$scope.step = 1;
+	$scope.adressLine = 1;
+
+	$scope.choice = {slice: 6, types, pates, extras};
+
 });
